fda6dc44067e050b64ee65d759088e06a0799d79
Sergio Manzanero <sergio.manzanero@gmail.com>
2018-04-28 19:09:24 +0200

    6. Recurso Quizzes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 
@@ -15,7 +16,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(partials());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +23,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method', {methods:["POST", "GET"]}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(partials());
 
 app.use('/', index);
 
